8bea360c19bb988587b713bdd41acdd9e8424bc4
Colum Ferry <cferry09@gmail.com>
2021-10-20 09:52:03 +0100

    setup

---
+++ libs/lib2/src/lib/lib2.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import './envs/env';
+
+@NgModule({
+  imports: [CommonModule],
+})
+export class Lib2Module {}
